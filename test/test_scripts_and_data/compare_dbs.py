943d529904350191ac26400b1916ef5ad50305e5
ilia Nikiforov <nikif002@umn.edu>
2024-03-25 16:05:41 -0500

    turn off additional floating point comparisons

--- test/test_scripts_and_data/compare_dbs.py
+++ test/test_scripts_and_data/compare_dbs.py
@@ -6,7 +6,7 @@ import os
 
 
 # TODO: more sophisticated checks for these keys
-KEYS_TO_SKIP = ["parameter-values","excess","relaxed-surface-positions"]
+KEYS_TO_SKIP = ["parameter-values","excess","relaxed-surface-positions","fit-error-max","fit-error-range"]
 
 
 def compare_db_to_reference(reference_json_path: str, test_db_path: str, float_fractional_tolerance: float = 0.01):
